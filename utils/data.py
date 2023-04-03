9e6f435ede2fd08c17d7817cff74ad0f78f7a7bb
Thorinka <mmlentyshenko@gmail.com>
2023-04-03 15:04:32 +0300

    add init, repr, get funcs to class Operations

--- utils/data.py
+++ utils/data.py
@@ -1,4 +1,13 @@
-class JsonFileOperation():
+class Operation:
+    """
+    Класс - Операция
+    Атрибуты: date - дата операции
+    state - статус операции
+    amount - сумма операции
+    description - описание операции
+    from_where - откуда совершена операция
+    to - куда была совершена операция
+    """
     def __init__(self, date, state, amount, description, from_where, to):
         self.date = date
         self.state = state
@@ -7,4 +16,30 @@ class JsonFileOperation():
         self.from_where = from_where
         self.to = to
 
-    def
+    def __repr__(self):
+        return f"Operation(date = {self.date}, " \
+               f"state = {self.state}, " \
+               f"amount = {self.amount}, " \
+               f"description = {self.description}, " \
+               f"from_where = {self.from_where}, " \
+               f"to = {self.to}"
+
+    def get_date(self):
+        return self.date
+
+    def get_state(self):
+        return self.state
+
+    def get_amount(self):
+        return self.amount
+
+    def get_description(self):
+        return self.description
+
+    def get_from_where(self):
+        return self.from_where
+
+    def get_to(self):
+        return self.to
+
+
--- utils/utils.py
+++ utils/utils.py
@@ -3,12 +3,22 @@ import pathlib
 
 
 def path(file):
+    """
+    Прописывает путь до файла
+    :param file: Имя файла
+    :return: Абсолютный путь
+    """
     dir_path = pathlib.Path.cwd()
     full_path = pathlib.Path(dir_path, "operation_files", file)
     return full_path
 
 
 def load_json_file(filename):
+    """
+    Читает и конвертирует json файл
+    :param filename: имя файла
+    :return: конвертированный файл (список словарей)
+    """
     with open(path(filename), "r", encoding="utf8") as json_file:
         converted_file = json.load(json_file)
         return converted_file
