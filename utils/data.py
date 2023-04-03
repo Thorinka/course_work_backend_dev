c0ebb4ad80a5d512520f76fd2701796c953511a2
Thorinka <mmlentyshenko@gmail.com>
2023-04-03 16:39:24 +0300

    add cypher_card_number function

--- utils/data.py
+++ utils/data.py
@@ -11,7 +11,8 @@ class Operation:
     from_where - откуда совершена операция
     to - куда была совершена операция
     """
-    def __init__(self, operation_date = None, state = None, amount = None, description = None, from_where = None, to = None):
+
+    def __init__(self, operation_date=None, state=None, amount=None, description=None, from_where=None, to=None):
         self.operation_date = operation_date
         self.state = state
         self.amount = amount
@@ -50,6 +51,18 @@ class Operation:
         new_date = date.fromisoformat(date_list[0])
         date_formatted = new_date.strftime("%d.%m.%Y")
         return date_formatted
+
+    def cypher_card_number(self):
+        card_number_list = self.get_from_where().split(" ")
+        unformatted_card_number = card_number_list[1]
+        new_unformatted_card_number = [unformatted_card_number[0:6], unformatted_card_number[6:12], unformatted_card_number[12:]]
+        formatting_list = [new_unformatted_card_number[0]]
+        for digit in new_unformatted_card_number[1]:
+            cyphered_digit = digit.replace(digit, "*")
+            formatting_list.append(cyphered_digit)
+        formatting_list.append(new_unformatted_card_number[2])
+        cyphered_card_number = "".join(formatting_list)
+        return " ".join([cyphered_card_number[i:i+4] for i in range(0, len(cyphered_card_number), 4)])
 
 
 
